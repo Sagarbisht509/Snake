96818940ea10719d45c1ff7a0a32d803b2ee916c
sagarbisht509 <sagarbisht509@gmail.com>
2024-03-23 21:04:49 +0530

    Changes on resources

--- app/src/main/java/com/sagar/snake/ui/theme/Type.kt
+++ app/src/main/java/com/sagar/snake/ui/theme/Type.kt
@@ -2,9 +2,15 @@ package com.sagar.snake.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.sagar.snake.R
+
+val honkFontFamily = FontFamily(
+    Font(R.font.honk_regular, FontWeight.Normal)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
